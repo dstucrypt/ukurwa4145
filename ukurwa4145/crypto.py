bb6033998c90fbdaae738382b25433ead691f81b
Ilya Petrov <ilya.muromec@gmail.com>
2014-05-14 23:59:26 +0300

    reject invalid params

--- ukurwa4145/crypto.py
+++ ukurwa4145/crypto.py
@@ -2,16 +2,28 @@ from .math import Field
 import random
 
 
+class ParamError(TypeError):
+    pass
+
+
 class Pubkey(object):
     def __init__(self, point):
         self.point = point
 
     def _help_check(self, value, s, r, domain=None):
 
+        if s == 0 or r == 0:
+            raise ValueError("Signature cannot be zero")
+
+        if s > domain.order or r > domain.order:
+            raise ValueError("Signature value cannot be grater than order")
+
         mulQ = self.point * r
         mulS = domain.base * s
 
         pointR = mulS + mulQ
+        if pointR.infinity:
+            raise ValueError("Invalid signature. R point is infinity")
 
         value = Field.truncate(value)
         r1 = Field.mul(value, pointR.x.v)
@@ -52,6 +64,10 @@ class Priv(object):
 
             return priv, pub
 
+    def pub(self, domain=None):
+        point_q = (domain.base * self.param_d).negate()
+        return Pubkey(point_q)
+
     def sign(self, value=None, value_hash=None, domain=None):
         if not value and not value_hash:
             raise ValueError("Nothing to sign")
@@ -59,20 +75,24 @@ class Priv(object):
         if not value_hash:
             raise ValueError("Hashing is not supported yet")
 
-        rand_e = random.randint(1, domain.order)
         truncated = Field.truncate(value_hash, domain.param_m)
-        return self._help_sign(truncated, rand_e, domain)
-
-    def pub(self, domain=None):
-        point_q = (domain.base * self.param_d).negate()
-        return Pubkey(point_q)
+        while True:
+            try:
+                rand_e = random.randint(1, domain.order)
+                return self._help_sign(truncated, rand_e, domain)
+            except ParamError:
+                pass
 
     def _help_sign(self, value, rand_e, domain):
         eG = domain.base * rand_e
+        if eG.x.v == 0:
+            raise ParamError("Random point have zero X coord")
 
         value = Field.truncate(value, domain.param_m)
         r = Field.mul(value, eG.x.v)
         r = Field.truncate(r)
+        if r == 0:
+            raise ParamError("Got zero R")
 
         s = (self.param_d * r) % domain.order
 
